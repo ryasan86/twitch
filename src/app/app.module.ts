294c4625e62bb09f7f8062ee5b4261ba4943d1cd
Ryan Santos <ryansantos86@gmail.com>
2018-07-25 17:46:07 -0700

    config.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchBoxComponent } from './search-box/search-box.component';
+import { SearchResultComponent } from './search-result/search-result.component';
+import { TwitchSearchComponent } from './twitch-search/twitch-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    SearchBoxComponent,
+    SearchResultComponent,
+    TwitchSearchComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/twitch-search/twitch-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-twitch-search',
+  templateUrl: './twitch-search.component.html',
+  styleUrls: ['./twitch-search.component.scss']
+})
+export class TwitchSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
